881a0454645b9b333528a6352295d68862542df0
Enes Günümdoğdu <enesgunumdogdu0@gmail.com>
2025-12-08 21:06:33 +0300

    Add RabbitMQ integration to ActivityService; configure RabbitMQ settings in application.yml and create RabbitMqConfig class for queue management. Introduce new data sources configuration for local MongoDB and PostgreSQL.

---
+++ activityservice/src/main/java/com/fitness/activityservice/config/RabbitMqConfig.java
@@ -0,0 +1,22 @@
+package com.fitness.activityservice.config;
+
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.support.converter.JacksonJsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+public class RabbitMqConfig {
+
+    @Bean
+    public Queue activityQueue(){
+        return new Queue("activity.queue", true);
+    }
+
+    @Bean
+    public MessageConverter jsonMessageConverter(){
+        return new JacksonJsonMessageConverter();
+    }
+}
