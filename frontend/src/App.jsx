93bb5179b7c35536de4c94e7f12f38a5a42d6765
Enes Günümdoğdu <enesgunumdogdu0@gmail.com>
2025-12-17 16:22:50 +0300

    Implement ActivitiesPage component in App.jsx, integrating ActivityForm and ActivityList for activity management. Add routing for activities and activity details, enhancing the application's navigation structure.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,21 @@
 import { BrowserRouter as Router, Navigate, Route, Routes, useLocation } from 'react-router';
-import { Button } from '@mui/material';
+import { Button, Box } from '@mui/material';
 import { useContext, useState, useEffect } from 'react';
 import { AuthContext } from 'react-oauth2-code-pkce';
 import { useDispatch } from 'react-redux';
 import { setCredentials } from './store/authSlice';
+import ActivityForm from './components/ActivityForm';
+import ActivityList from './components/ActivityList';
+import ActivityDetail from './components/ActivityDetail';
+
+const ActivitiesPage = () => {
+  return (
+    <Box component = "section" sx={{ p: 2 , border: '1px dashed grey'}}>
+      <ActivityForm onActivitiesAdded={() => window.location.reload()} />
+      <ActivityList />
+    </Box>
+  );
+}
 
 
 function App() {
@@ -25,10 +37,20 @@ function App() {
         Login
       </Button>
       ) : (
+        /*
         <div>
           <pre>{JSON.stringify(tokenData, null, 2)}</pre>
           <pre>{JSON.stringify(token, null, 2)}</pre>
         </div>
+        */
+         <Box component = "section" sx={{ p: 2 , border: '1px dashed grey'}}>
+          <Routes>
+            <Route path= "/activities" element ={<ActivitiesPage />}/>
+            <Route path= "/activities/:id" element ={<ActivityDetail />}/>
+
+            <Route path="/" element = {token ? <Navigate to="/activities" replace/> : <div>Welcome! Please login</div>} />
+          </Routes>
+         </Box>
       )}
     </Router>
   );
