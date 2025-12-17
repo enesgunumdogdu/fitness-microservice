3c4f307a06384038cb828df02ae76ba59e33ff53
Enes Günümdoğdu <enesgunumdogdu0@gmail.com>
2025-12-17 13:21:14 +0300

    Add react-oauth2-code-pkce for authentication and integrate it into the App component. Update authSlice to manage user credentials and local storage. Refactor main.jsx to include AuthProvider for context management.

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-
+import { AuthProvider } from 'react-oauth2-code-pkce'
+import { authConfig } from './authConfig'
 import { Provider } from 'react-redux'
 import { store } from './store/store'
 
@@ -9,7 +10,10 @@ import App from './App'
 // As of React 18
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+  <AuthProvider authConfig={authConfig}
+                loadingComponent={<div>Loading...</div>}>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </AuthProvider>,
 )
