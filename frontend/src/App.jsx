a6c2391f3777284fca2a3bb5932d49c6abb91fa0
Enes Günümdoğdu <enesgunumdogdu0@gmail.com>
2025-12-17 12:22:16 +0300

    Update frontend dependencies and refactor App component to integrate Material-UI and React Router. Add Redux store provider for state management.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,16 @@
-
+import { BrowserRouter as Router, Navigate, Route, Routes, useLocation } from 'react-router';
+import { Button } from '@mui/material';
 
 function App() {
 
 
   return (
-    <>
-      
-    </>
-  )
+    <Router>
+      <Button variant="contained" color="#dc004e" >
+        Login
+      </Button>
+    </Router>
+  );
 }
 
-export default App
+export default App;
