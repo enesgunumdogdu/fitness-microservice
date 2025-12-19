74d35ab7651235723db3609474ad926792ca66f1
Enes Günümdoğdu <enesgunumdogdu0@gmail.com>
2025-12-19 22:37:53 +0300

    Update RabbitMQ configuration to use Jackson2JsonMessageConverter instead of JacksonJsonMessageConverter in activityservice and aiservice.

--- activityservice/src/main/java/com/fitness/activityservice/config/RabbitMqConfig.java
+++ activityservice/src/main/java/com/fitness/activityservice/config/RabbitMqConfig.java
@@ -4,7 +4,7 @@ import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.DirectExchange;
 import org.springframework.amqp.core.Queue;
-import org.springframework.amqp.support.converter.JacksonJsonMessageConverter;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -40,6 +40,6 @@ public class RabbitMqConfig {
 
     @Bean
     public MessageConverter jsonMessageConverter(){
-        return new JacksonJsonMessageConverter();
+        return new Jackson2JsonMessageConverter();
     }
 }
