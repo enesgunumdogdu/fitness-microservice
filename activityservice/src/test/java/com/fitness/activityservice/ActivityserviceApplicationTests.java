28c89c79631b0c01a17be6aaac04ba653b939486
Enes Günümdoğdu <enesgunumdogdu0@gmail.com>
2025-12-29 17:16:00 +0300

    Add CI-friendly tests for all microservices
    - Add test profiles with mocked external dependencies
    - Mock JWT decoder, Keycloak, MongoDB, RabbitMQ
    - Add H2 in-memory database for UserService tests
    - Disable Eureka client and Config server in test mode

--- activityservice/src/test/java/com/fitness/activityservice/ActivityserviceApplicationTests.java
+++ activityservice/src/test/java/com/fitness/activityservice/ActivityserviceApplicationTests.java
@@ -1,13 +1,31 @@
 package com.fitness.activityservice;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.ApplicationContext;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class ActivityserviceApplicationTests {
+
+	@MockBean
+	private MongoTemplate mongoTemplate;
+
+	@MockBean
+	private RabbitTemplate rabbitTemplate;
+
+	@Autowired
+	private ApplicationContext applicationContext;
 
 	@Test
 	void contextLoads() {
+		assertThat(applicationContext).isNotNull();
 	}
-
 }
